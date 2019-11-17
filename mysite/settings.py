04ca5f9269dea8bb45cff40407faed45ab0a0d74
Tushar <tushar@Admins-MacBook-Pro.local>
2019-11-17 23:57:11 +0000

    updated settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ TEMPLATES_DIRS= os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!ys9xpe-_7$f044bz_08ycyy6nk!qbwu1-%3aw3&5hu_r1huyr'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'info.dgtit@gmail.com'
-EMAIL_HOST_PASSWORD = 'Zaq12wsx$'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
